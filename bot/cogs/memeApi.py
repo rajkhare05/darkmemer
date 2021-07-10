096e8fff4eb1478e41e82922696dbd32605a6fed
rajkhare05 <06rajkhare04@gmail.com>
2021-07-10 20:18:31 +0530

    meme queue fixed

--- bot/cogs/memeApi.py
+++ bot/cogs/memeApi.py
@@ -10,7 +10,7 @@ class pull_memes(commands.Cog):
 		self.AFTER = int(datetime(d.year, d.month, d.day, 0, 0, 0).timestamp())
 		self.BEFORE = self.AFTER + 3600
 		self.LINK = "https://apiv2.pushshift.io/reddit/submission/search?subreddit=memes"\
-					"&limit=100"\
+					"&limit=20"\
 					"&after={0}"\
 					"&before={1}".format(self.AFTER, self.BEFORE)
 		data = {}
@@ -19,39 +19,47 @@ class pull_memes(commands.Cog):
 		memes = reponse.json()
 		with open('./bot/cogs/memes.json', 'w') as file:
 			for meme in memes['data']:
-				data['memes'].append({
-					'title': meme['title'],
-					'url': meme['url'],
-					'permalink': meme['permalink']
-				})
-			print('len: ', len(data['memes']))
-			json.dump(data, file, indent = 4)
-		self.memes = self.get_meme()
-
-	def set_link(self):
-		self.AFTER = self.BEFORE
-		self.BEFORE = self.BEFORE + 3600
-		self.LINK = "https://apiv2.pushshift.io/reddit/submission/search?subreddit=memes"\
-					"&limit=50"\
-					"&after={0}"\
-					"&before={1}".format(self.AFTER, self.BEFORE)
-
-	def add_more_memes(self):
-		with open('./bot/cogs/memes.json', 'r+') as file:
-				data = json.load(file)
-				self.set_link()
-				memes = requests.get(self.LINK).json()
-				for meme in memes['data']:
+				if self.validity(meme):
 					data['memes'].append({
 						'title': meme['title'],
 						'url': meme['url'],
 						'permalink': meme['permalink']
 					})
-				print('len: ', len(data['memes']))
-				file.seek(0)
-				json.dump(data, file, indent = 4)
-		return
-	
+			print('len: ', len(data['memes']))
+			json.dump(data, file, indent = 4)
+		self.memes = self.get_meme()
+
+	def update_link(self):
+		self.AFTER = self.BEFORE
+		self.BEFORE = self.BEFORE + 3600
+		self.LINK = "https://apiv2.pushshift.io/reddit/submission/search?subreddit=memes"\
+					"&limit=20"\
+					"&after={0}"\
+					"&before={1}".format(self.AFTER, self.BEFORE)
+
+	def add_more_memes(self):
+		with open('./bot/cogs/memes.json', 'r+') as file:
+			data = json.load(file)
+			self.update_link()
+			memes = requests.get(self.LINK).json()
+			for meme in memes['data']:
+				if self.validity(meme):
+					self.memes['memes'].append({
+						'title': meme['title'],
+						'url': meme['url'],
+						'permalink': meme['permalink']
+					})
+			print('len: ', len(self.memes['memes']))
+			file.seek(0)
+			json.dump(self.memes, file, indent = 4)
+
+	def validity(self, data: dict):
+		if not 'removed_by_category' in data.keys():
+			return True
+		elif data['removed_by_category'] == 'reddit':
+			return True
+		return False
+
 	def delete_meme(self):
 		with open('./bot/cogs/memes.json', 'w') as file:
 			self.memes['memes'].pop(0)
@@ -73,7 +81,7 @@ class pull_memes(commands.Cog):
 		)
 		embed_.set_image(url = self.memes['memes'][0]['url'])
 		self.delete_meme()
-		if len(self.memes['memes']) < 50:
+		if len(self.memes['memes']) < 2:
 			self.add_more_memes()
 		return await ctx.send(embed = embed_)
 
